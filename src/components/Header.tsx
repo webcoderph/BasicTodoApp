6df9cb910136532a5ce09e0270a6f3d057fba0cd
Maynard <webcoderph@gmail.com>
2025-04-11 11:50:58 +0800

    change name

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -4,7 +4,7 @@ const Header = () => {
       className="bg-indigo-500 w-full"
       style={{ height: "10vh", paddingTop: "2.5vh" }}
     >
-      <h1 className="text-2xl font-bold text-white text-center">Todo List</h1>
+      <h1 className="text-2xl font-bold text-white text-center">KatKat</h1>
       <ul className="list-none text-center text-white">
         <li className="inline-block mx-2">
           <a href="/">Home</a>
